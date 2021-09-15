5579e79e4d70236ebb9d9d44312a83ee53b27a9f
Noah Birnel <nbirnel@gmail.com>
2021-09-15 12:25:14 -0700

    update most docstrings to rtd style

--- testaws/__init__.py
+++ testaws/__init__.py
@@ -28,6 +28,8 @@ def uppercase_initial(string):
     :param string: Input string
     :type string: str
     :return: Capitalized input string
+    :rtype: str
+
     >>> uppercase_initial("disableApiTermination")
     "DisableApiTermination"
     """
@@ -37,25 +39,84 @@ def uppercase_initial(string):
 
 def match_env_type_num_name_scheme(objects, infix, env=r"^[^-]+-", num=r"-[0-9][0-9]$"):
     """
-    Accept objects (iterable of aws objects with Tags key),
-    infix (raw string for use as regex),
-    optional env and num (raw strings for use as regex:
-    default to r"^[^-]+-" and r"-[0-9][0-9]$").
     Return objects with a Name tag matching the regex
-    env-infix-num.
+    (env)(infix)(num)
+
     Example: prod-web-01
+
+    This wraps objects_tags_key_values_matches_regex.
+
+    :param objects: Iterable of aws objects with a Tags key
+    :type objects: iterable
+    :param infix: Raw string for use as regex
+    :type objects: str
+    :param env: Raw string for use as regex.
+        Defaults to r"^[^-]+-" 
+    :type env: str, optional
+    :param num: Raw string for use as regex.
+        defaults to r"-[0-9][0-9]$").
+    :type num: str, optional
+    :return: List of returned boto3 objects
+    :rtype: list
     """
     regex = re.compile(env + infix + num)
     return objects_tags_key_values_matches_regex(objects, "Name", regex)
 
 
+def objects_tags_key_values_matches_regex(objects, key, regex):
+    """
+    Return objects with tagged with key matching regex.
+    You may wish to use match_env_type_num_name_scheme instead when possible.
+
+    :param objects: Iterable of aws objects with a Tags key
+    :type objects: iterable
+    :param key: Tag to compare against
+    :type key: str
+    :param regex: Regex to match
+    :type regex: re
+    :return: List of returned boto3 objects
+    :rtype: list
+    """
+    return [
+        obj
+        for obj in objects
+        if tags_key_value_matches_regex(obj, key, regex)
+    ]
+
+
+def tags_key_value_matches_regex(aws_object, key, regex):
+    """
+    Return true if aws_object's key key matches regex, 
+    otherwise False.
+
+    :param aws_object: A boto3 aws object to check
+    :param key: Tag to compare against
+    :type key: str
+    :param regex: Regex to match
+    :type regex: re
+    :return: True or False, if there was a match
+    :rtype: bool
+    """
+    tags = aws_object["Tags"]
+    return any(
+        tag
+        for tag in tags
+        if tag["Key"] == key and regex.match(tag["Value"])
+    )
+
+
 @lru_cache(maxsize=128)
 def ask_terraform(query):
     """
-    Accept a freeform terraform console query.
-    Return terraform's answer.
+    Ask terraform console a question.
     When possible,
     it is easier to use terraform_data or terraform_variable instead.
+    Use terraform_struct instead if the output will be used as a data structure.
+
+    :param query: terraform console expression
+    :type query: str
+    :return: Terraform's output
+    :rtype: str
     """
     tf_console = ["terraform", "console"]
     tf = subprocess.run(
@@ -65,8 +126,12 @@ def ask_terraform(query):
 
 def terraform_output(query):
     """
-    Accept a terraform output query.
-    Return terraform's answer.
+    Ask terraform output a question.
+
+    :param query: terraform output key
+    :type query: str
+    :return: Terraform's output
+    :rtype: str
     """
     tf_output = ["terraform", "output", query]
     tf = subprocess.run(
@@ -76,51 +141,74 @@ def terraform_output(query):
 
 def terraform_value(what_type, name):
     """
-    Accept what_type (str, 'data' or 'var'),
-    and name (str).
-    Return terraform's understanding of the data or variable.
-    It is usually easier to use terraform_data or terraform_variable instead.
+    Ask terraform console for a data or a variable value.
+    When possible,
+    it is easier to use terraform_data or terraform_variable instead.
+
+    :param what_type: "data" or "var"
+    :type what_type: str
+    :param name: Name of data or var to return
+    :type name: str
+    :return: Terraform's output
+    :rtype: str
     """
+    if what_type not in ['data', 'var']:
+        raise ValueError
     query = f"{what_type}.{name}"
     return ask_terraform(query)
 
 def terraform_data(data):
     """
-    Accept data (name of a terraform data object).
-    Return terraform's value for that data object.
+    Ask terraform console for a data value.
+
+    :param data: terraform data to look up
+    :type data: str
+    :return: Terraform's output
+    :rtype: str
     """
     return terraform_value('data', data)
 
 
 def terraform_variable(var):
     """
-    Accept var (name of a terraform variable).
-    Return terraform's value for that variable.
+    Ask terraform console for a variable value.
+
+    :param var: terraform var to look up
+    :type var: str
+    :return: Terraform's output
+    :rtype: str
     """
     return terraform_value('var', var)
 
 
 def terraform_struct(query):
     """
-    Accept a terraform query.
-    Return terraform's value for that variable as a data structure.
+    Ask terraform console a question,
+    returning the answer as a data structure
     (list or dict, as appropriate)
 
+    :param query: terraform console expression
+    :type query: str
+    :return: Terraform's output
+    :rtype: list or dict
     """
     return yaml.safe_load(ask_terraform(query))
 
 
 def get_security_groups(filters=[]):
     """
-    Accept (optional) filters (list).
-    Return security groups matching those filters. 
+    Return security groups matching filter.
     See 
     https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_DescribeSecurityGroups.html
     and 
     https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Client.describe_security_groups
     for details on available filters.
 
-    >>> filters = [{"Name": "vpc-id", "Values": ["vpc-0123456789abcdef0"]}]
+    :param filters: Filters to apply.
+    :type filters: list of dicts, optional.
+        for example: filters = [{"Name": "vpc-id", "Values": ["vpc-0123456789abcdef0"]}]
+    :return: set of security groups
+    :rtype: set
     """
     return ec2_client.describe_security_groups(Filters=filters)["SecurityGroups"]
 
@@ -130,6 +218,15 @@ def get_load_balancers():
 
 
 def get_instances(filters=[]):
+    """
+    Return instances matching filter.
+
+    :param filters: Filters to apply.
+    :type filters: list of dicts, optional.
+        for example: filters = [{"Name": "vpc-id", "Values": ["vpc-0123456789abcdef0"]}]
+    :return: list of instances
+    :rtype: list
+    """
     reservations = ec2_client.describe_instances(Filters=filters)["Reservations"]
     return list(chain.from_iterable(r["Instances"] for r in reservations))
 
@@ -143,28 +240,14 @@ def get_s3_buckets_names():
         for bucket in s3_client.list_buckets()['Buckets']
     ]
 
-def objects_tags_key_values_matches_regex(objects, key, regex):
-    return [
-        obj
-        for obj in objects
-        if tags_key_value_matches_regex(obj, key, regex)
-    ]
-
-
-def tags_key_value_matches_regex(aws_object, key, regex):
-    tags = aws_object["Tags"]
-    return any(
-        tag
-        for tag in tags
-        if tag["Key"] == key and regex.match(tag["Value"])
-    )
-
-
 def instances_security_groups_ids(instances):
     """
-    Accept instances (list of instance objects).
-    Return all security group ids for those instances
-    as a set.
+    Return the set of security group IDs applied to instances.
+
+    :param instances: list of ec2 instance objects
+    :type instances: list
+    :return: set of security group IDs applied to instances
+    :rtype: set
     """
     return set(
         group["GroupId"] for group in instances_security_groups(instances)
@@ -173,9 +256,12 @@ def instances_security_groups_ids(instances):
 
 def instances_security_groups(instances):
     """
-    Accept instances (list of instance objects).
-    Return all security groups for those instances,
-    as a list of dicts with keys GroupId and GroupName.
+    Return security groups associated with instances.
+
+    :param instances: list of ec2 instance objects
+    :type instances: list
+    :return: list of dicts with keys GroupId and GroupName
+    :rtype: list
     """
     # we turn the groups into frozensets to make them hashable,
     # so we can use set to deduplicate.
@@ -190,8 +276,12 @@ def instances_security_groups(instances):
 
 def security_groups_ingress(group_ids):
     """
-    Accept group_ids (list).
-    Return all ingress rules for those groups as a list.
+    Return all ingress rules for a list of security group IDs
+
+    :param group_ids: list of security group IDs
+    :type group_ids: list
+    :return: list of security group ingress rules
+    :rtype: list
     """
     groups = [ec2.SecurityGroup(gid) for gid in group_ids]
     return [rule for group in groups for rule in group.ip_permissions]
@@ -199,8 +289,12 @@ def security_groups_ingress(group_ids):
 
 def security_groups_egress(group_ids):
     """
-    Accept group_ids (list).
-    Return all egress rules for those groups as a list.
+    Return all egress rules for a list of security group IDs
+
+    :param group_ids: list of security group IDs
+    :type group_ids: list
+    :return: list of security group egress rules
+    :rtype: list
     """
     groups = [ec2.SecurityGroup(gid) for gid in group_ids]
     return [
@@ -212,8 +306,12 @@ def security_groups_egress(group_ids):
 
 def rules_ports(rules):
     """
-    Accept rules (list).
-    Return set of ports covered by those rules.
+    Return set of ports covered by a list of security group rules.
+
+    :param rules: list of security group rules
+    :type rules: list
+    :return: set of ports
+    :rtype: set
     """
     return set(
         port
@@ -224,9 +322,13 @@ def rules_ports(rules):
 
 def port_in_rule(port, rule):
     """
-    Accept port (int) and rule.
-    Return True if port is covered by the rule.
-    Return False otherwise.
+    Return True or False if port is covered by a security group rule.
+
+    :param port: port to check
+    :type port: int
+    :param rule: security group rule to check
+    :return: True or False if port is covered by a security group rule.
+    :rtype: bool
     """
     try:
         return port in range(rule["FromPort"], rule["ToPort"] + 1)
@@ -236,8 +338,12 @@ def port_in_rule(port, rule):
 
 def instances_ingress_rules(instances):
     """
-    Accept instances (list)
-    Return ingress rules
+    Return all ingress rules for a list of instances.
+
+    :param instances: list of instances
+    :type instances: list
+    :return: list of security group ingress rules
+    :rtype: list
     """
     sg_ids = instances_security_groups_ids(instances)
     return security_groups_ingress(sg_ids)
@@ -245,8 +351,12 @@ def instances_ingress_rules(instances):
 
 def instances_egress_rules(instances):
     """
-    Accept instances (list)
-    Return egress rules
+    Return all egress rules for a list of instances.
+
+    :param instances: list of instances
+    :type instances: list
+    :return: list of security group egress rules
+    :rtype: list
     """
     sg_ids = instances_security_groups_ids(instances)
     return security_groups_egress(sg_ids)
@@ -254,8 +364,12 @@ def instances_egress_rules(instances):
 
 def instances_ingress_ports(instances):
     """
-    Accept instances (list)
-    Return set of ingress ports
+    Return all allowed ingress ports for a list of instances.
+
+    :param instances: list of instances
+    :type instances: list
+    :return: set of allowed ingress ports
+    :rtype: set
     """
     rules = instances_ingress_rules(instances)
     return rules_ports(rules)
@@ -263,8 +377,12 @@ def instances_ingress_ports(instances):
 
 def instances_egress_ports(instances):
     """
-    Accept instances (list)
-    Return set of egress ports
+    Return all allowed egress ports for a list of instances.
+
+    :param instances: list of instances
+    :type instances: list
+    :return: set of allowed egress ports
+    :rtype: set
     """
     rules = instances_egress_rules(instances)
     return rules_ports(rules)
@@ -272,8 +390,14 @@ def instances_egress_ports(instances):
 
 def instances_egress_rules_for_port(instances, port):
     """
-    Accept instances (list) and port (int).
-    Return egress rules which include port
+    Return egress rules applied to instances which include port.
+
+    :param instances: list of instances
+    :type instances: list
+    :param port: port
+    :type port: int
+    :return: list of egress rules
+    :rtype: list
     """
     sg_ids = instances_security_groups_ids(instances)
     rules = security_groups_egress(sg_ids)
@@ -286,8 +410,14 @@ def instances_egress_rules_for_port(instances, port):
 
 def instances_ingress_rules_for_port(instances, port):
     """
-    Accept instances (list) and port (int).
-    Return ingress rules which include port
+    Return ingress rules applied to instances which include port.
+
+    :param instances: list of instances
+    :type instances: list
+    :param port: port
+    :type port: int
+    :return: list of ingress rules
+    :rtype: list
     """
     sg_ids = instances_security_groups_ids(instances)
     rules = security_groups_ingress(sg_ids)
@@ -300,8 +430,13 @@ def instances_ingress_rules_for_port(instances, port):
 
 def rules_cidrs_and_security_groups(rules):
     """
-    Accept rules (list).
-    Retrun dict with keys 'cidrs' and 'sgids'
+    Return a dict with keys "cidrs" and "sgids" 
+    from a list of security group rules.
+
+    :param rules: list of security group rules
+    :type rules: list
+    :return: Dict with keys "cidrs" and "sgids"
+    :rtype: dict
     """
     cidrs = set(
         ip_range["CidrIp"]
@@ -318,9 +453,15 @@ def rules_cidrs_and_security_groups(rules):
 
 def instances_port_ingress_sources(instances, port):
     """
-    Accept instances (list) and port (int).
-    Return dict with keys 'cidrs' and 'sgids' of sources that can reach port
-    on instances.
+    Return dict with keys 'cidrs' and 'sgids'
+    of sources that can reach port on instances.
+
+    :param instances: list of instances
+    :type instances: list
+    :param port: port
+    :type port: int
+    :return: Dict with keys "cidrs" and "sgids", of network sources
+    :rtype: dict
     """
     rules = instances_ingress_rules_for_port(instances, port)
     return rules_cidrs_and_security_groups(rules)
@@ -328,11 +469,18 @@ def instances_port_ingress_sources(instances, port):
 
 def instances_attribute(instances, attribute):
     """
-    Accept instances (list) and attribute (str).
-    Return list that attributes value for the instances.
+    Return a list of the indicated attribute values for instances.
+
     See
     https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.ec2_client.describe_instance_attribute
     for usable attributes.
+
+    :param instances: list of instances
+    :type instances: list
+    :param attributes: attribute to look up
+    :type attributes: str
+    :return: list of attribute values
+    :rtype: list
     """
     capitalized_attribute = uppercase_initial(attribute)
 
@@ -357,8 +505,12 @@ def instances_elastic_ips(instances):
 
 def buckets_encrypted(buckets):
     """
-    Accept a list of aws bucket names
     Return bucket's encryption object or None for each bucket.
+
+    :param buckets: list of buckets
+    :type buckets: list
+    :return; list of encryption object / None
+    :rtype: list
     """
 
     def maybe_encrypted(bucket):
